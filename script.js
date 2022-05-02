80988ed3882321ebecdc0a4ff58f9cf395352089
parthmadhvani <parthmadhvani132@gmail.com>
2022-05-03 00:27:55 +0530

    Click added

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
-var gameOver = false;
+
 var arrBlink = [];
 var arrClick = [];
 var level = 0;
 
 $(document).on("keypress", function () {
-    if (level==0) {
+    if (level == 0) {
         blink();
     }
 })
 
 $("button").on("click", function () {
-    var clickBtnId = $(this).attr("id");
-    arrClick.push(clickBtnId);
-    check(arrClick.length-1);
-    $(this).addClass("animation")
-    setTimeout(function () {
-        $("button").removeClass("animation")
-    },50)
+    if (level == 0) {
+        setTimeout(blink,300);
+    }
+    else {
+        var clickBtnId = $(this).attr("id");
+        arrClick.push(clickBtnId);
+        check(arrClick.length - 1);
+        $(this).addClass("animation")
+        setTimeout(function () {
+            $("button").removeClass("animation")
+        }, 50)
+    }
 })
 
+
+
 function blink() {
+
     arrClick = [];
     level++;
-    $("p").text("Level "+level)
+    $("p").text("Level " + level)
     var i = Math.floor(Math.random() * 4);
     var flashBtnId = '#btn' + i;
     arrBlink.push(flashBtnId.slice(1, 5))
@@ -32,12 +40,12 @@ function blink() {
 function check(current) {
     if (arrClick[current] === arrBlink[current]) {
         if (arrClick.length === arrBlink.length) {
-            setTimeout(blink,1000)
+            setTimeout(blink, 1000)
         }
+        gameOver = false;
     }
-    
-    else{
-        gameOver = true;
+
+    else {
         gameover();
     }
 }
@@ -45,10 +53,12 @@ function check(current) {
 function gameover() {
     arrBlink = [];
     level = 0;
+    gameOver = true;
+
     $("body").addClass("gameover");
     $("p").text("Game over")
-    setTimeout(function(){
-        $("p").text("Press any key to start...")
+    setTimeout(function () {
+        $("p").text("Press anything to start...")
         $("body").removeClass("gameover");
-    },1200)
+    }, 1200)
 }
